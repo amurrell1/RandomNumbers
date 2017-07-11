036c62a9552c4a807ef46bdbe3f74d2dc8e4c51d
Andre Murrell <andre.murrell@outlook.com>
2017-07-11 10:02:16 -0400

    Add files via upload

---
+++ RandomizeNumbers/RandomPasswordGenerator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomPasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static string GeneratePassword()
+        {
+            string myPwdList = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string myActualPwd = "";
+            Random random = new Random();
+            for (int i = 0; i <= 7; i++)
+            {
+                int rand = random.Next(5, myPwdList.Length - 1);
+                myActualPwd += myPwdList.Substring(rand, 1);
+            }
+            return myActualPwd;
+        }
+    }
+}
---
+++ RandomizeNumbers/RandomizeNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomizeNumbers
+{
+    class Program
+    {
+        //created the first obj of random above main method and then called next on it
+        //static Random obj = new Random(); //seeding from CPU clock
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPseudoRandom(2));
+            Console.WriteLine(GetPseudoRandom(23));
+            Console.WriteLine(GetPseudoRandom(3));
+            Console.WriteLine(GetPseudoRandom(5));
+            Console.WriteLine(GetPseudoRandom(6));
+        }
+        //created a static method GetPseudoRandom and had it return an obj to a string.
+        private static string GetPseudoRandom(int seed) //creating a seed inside of the method)
+        {
+            Random obj = new Random(seed);
+            return obj.Next(4, 50).ToString();
+        }
+            /*
+            //this is my pseudo random attempt (not really random if you could figure out the see)
+            Random random = new Random();
+            Console.WriteLine(random.Next());
+            */
+            /*
+            int number = random.Next(1, 13);
+            int number2 = random.Next(1000);
+        }
+        private int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+        */
+        }
+    }
+
+
